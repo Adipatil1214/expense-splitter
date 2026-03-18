6fdc67e98f5ec47663e0445a3fe87235298782ee
Adipatil1214 <p.aditya1412@gmail.com>
2026-03-18 11:54:42 +0530

    Added background image

--- backend/add_vendor.py
+++ backend/add_vendor.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("backend/database.db")
 cursor = conn.cursor()
 
 cursor.execute("DELETE FROM receipt;")
-cursor.execute("DELETE FROM sqlite_sequence WHERE name='vendor';")
+cursor.execute("DELETE FROM sqlite_sequence WHERE name='receipt';")
 
 conn.commit()   # VERY IMPORTANT
 
